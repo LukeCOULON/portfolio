4ce7ea97122742887c744d9d66e1bebc7b94c029
LukeCOULON <lukecoulon@gmail.com>
2026-08-27 21:25:24 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,10 @@ const GITHUB_USER = "LukeCOULON";
 
 const EXCLUDE_REPOS = [
 https://github.com/LukeCOULON/tools
+https://github.com/LukeCOULON/reporeader
+https://github.com/LukeCOULON/hy
+https://github.com/LukeCOULON/thomasfit
+
 ];
 
 const DESCRIPTION_OVERRIDES = {
